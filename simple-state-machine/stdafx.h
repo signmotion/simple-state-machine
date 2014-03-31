dbb5c573f59a0f930bdd1d71b6209b8f6e81023e
Andrey Syrokomsky <signmotion@gmail.com>
2014-03-31 17:21:22 +0300

    First commit.

---
+++ simple-state-machine/stdafx.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <iostream>
+
+// макс. кол-во состояний для конечного автомата
+#define FUSION_MAX_VECTOR_SIZE 10
+
+
+// back-end
+#include <boost/msm/back/state_machine.hpp>
+//front-end
+#include <boost/msm/front/state_machine_def.hpp>
+
+
+namespace msm  = boost::msm;
+namespace msmf = boost::msm::front;
+namespace mpl  = boost::mpl;
